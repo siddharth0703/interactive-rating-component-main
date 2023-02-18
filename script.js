df170250e834d039b8d4347bf3a37167c2c61621
Siddharth Indora <siddharth.svr@gmail.com>
2023-02-17 22:12:43 -0800

    Modified script.js with windows.location

--- script.js
+++ script.js
@@ -13,11 +13,6 @@ for (let i = 0;i<rating_list.children.length;i++){
 let submit_btn = document.getElementById("submit-btn");
 submit_btn.addEventListener("click" ,() => {
     console.log("Submit was clicked")
-    f1();
-    window.location.href = "/landingpage.html?rating="+rating.toString();
+    window.location.href = "landingpage.html?rating="+rating.toString();
 }
 )
-
-function f1 (){
-    console.log("rating is now",rating)
-}
